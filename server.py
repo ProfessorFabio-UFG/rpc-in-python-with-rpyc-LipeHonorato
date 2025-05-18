9de07aabd3b1b39f6ec0d7e13dea7bbc85fd612c
Felipe Honorato <99209704+LipeHonorato@users.noreply.github.com>
2025-05-18 18:27:55 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import rpyc
 from constRPYC import * #-
 from rpyc.utils.server import ThreadedServer
+import random
 
 class DBList(rpyc.Service):
   value = []
@@ -11,6 +12,22 @@ class DBList(rpyc.Service):
 
   def exposed_value(self):
     return self.value
+
+  def exposed_accumulator(self):
+    self.accumulator = 0
+    for i in range(len(self.value)):
+      self.accumulator += self.value[i]
+    return self.accumulator
+
+  def exposed_sum(self, value1, value2):
+    return value1 + value2
+
+  def exposed_loadedDice(self):
+    if random.randint(1,10) == 3:
+      return random.randint(1,6)
+    else: 
+      return 4
+    
 
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
